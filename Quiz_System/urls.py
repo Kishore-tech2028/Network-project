6ad0c88ace0b8a37bb2084eec45daeeb9c36b384
Kishore <188225831+Kishore-tech2028@users.noreply.github.com>
2026-03-23 21:03:59 +0530

    Added frontend

--- Quiz_System/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("api/score/", views.get_live_score, name="api_score"),
-    path("Quiz_System", views.home, name="quiz_system_home"),    path("Quiz_System/", views.home, name="quiz_system_home_slash"),
-]
